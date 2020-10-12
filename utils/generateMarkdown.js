0e765925fb7d6e0be97ce863b0919633c355c7f5
Sean Kempf <poserposer@me.com>
2020-10-11 17:44:28 -0700

    Created the layout for the way the README will be displayed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,36 @@ function generateMarkdown(data) {
   # ${data.title}
   ![License](https://img.shields.io/badge/License-${data.license}-brightgreen.svg)
 
-  
+  ## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits / Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+
+
+## Installation 
+${data.install}
+
+## Usage
+${data.usage}
+
+## Credits / Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.github}
+<br>
+${data.email}
+
+## License 
+${data.license}
 `;
 }
 
