5330e4fe3326e5b6ebfe4ee1d75fff13f645b641
Sean Kempf <poserposer@me.com>
2020-10-13 18:27:10 -0700

    Logged out the responses from the user to connect to the rest of my functions, wrote out an 'if' statement that will determine what license is displayed

--- index.js
+++ index.js
@@ -3,73 +3,81 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown.js");
+const { resolve } = require("path");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // Creating a function for the User Questions
-function promptUser(){
-// array of questions for user
-return inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of this application?"
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Give a description of the application"
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "How does a user install this application?"
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "How does a user use this application?"
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "What license does this application use?",
-    choices: ["Apache", "MIT", "GNU GPLv3"]
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "How does someone contribute to this project?"
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "What tests did you run on the application?"
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is your GitHub?"
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What email do you want people to contact you with questions?"
-  },
-]);
+function promptUser() {
+  // array of questions for user
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of this application?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Give a description of the application"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "How does a user install this application?"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "How does a user use this application?"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What license does this application use?",
+        choices: ["Apache", "MIT", "GNU GPLv3"]
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "How does someone contribute to this project?"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What tests did you run on the application?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What email do you want people to contact you with questions?"
+      },
+    ])
+    .then(res => {
+console.log(res)
+    });
+
 }
 
+
+
 // function to write README file
-function writeToFile(fileName, data) {
-  
-writeFileAsync(fileName, data)
-let fileName = "README.md";
-let data = generateMarkdown.generateMarkdown(answers)
+async function writeToFile(fileName, data) {
+
+  // await writeFileAsync(fileName, data)
+
 }
 
 // function to initialize program
 function init() {
-writeToFile();
+  promptUser();
+  // writeToFile("README.md", generateMarkdown(), "utf8");
 }
 
 // function call to initialize program
